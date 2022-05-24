029812f621ff85815c5a0a7ab4e2f105b7b05d99
Ajjig <majjig@pm.me>
2022-05-24 03:26:01 +0100

    remove printing std::endl

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -4,38 +4,41 @@ int main(int ac, char **av) {
     int choice;
     PhoneBook phoneBook = PhoneBook();
     while (true) {
+        std::cout << "===========================" << std::endl;
         std::cout << "Choose an option: " << std::endl;
         std::cout << "[ 1 ] Add a contact" << std::endl;
         std::cout << "[ 2 ] Search a contact" << std::endl;
-        std::cout << "[ 0 ] Exit" << std::endl;
+        std::cout << "[ 3 ] Exit" << std::endl;
+        std::cout << "===========================" << std::endl;
         std::cout << "Choice: ";
         std::cin >> choice;
-        if (choice == 0)
+        if (choice == 3)
             phoneBook.exit();
         else if (choice == 1) {
-            std::cout << "Enter the first name: " << std::endl;
+            std::cout << "Enter the first name: ";
             std::string firstName;
             std::cin >> firstName;
-            std::cout << "Enter the last name: " << std::endl;
+            std::cout << "Enter the last name: ";
             std::string lastName;
             std::cin >> lastName;
-            std::cout << "Enter the nickname: " << std::endl;
+            std::cout << "Enter the nickname: ";
             std::string nickName;
             std::cin >> nickName;
-            std::cout << "Enter the phone number: " << std::endl;
+            std::cout << "Enter the phone number: ";
             std::string phoneNumber;
             std::cin >> phoneNumber;
-            std::cout << "Enter the darkest secret: " << std::endl;
+            std::cout << "Enter the darkest secret: ";
             std::string darkestSecret;
             std::cin >> darkestSecret;
             Contact contact(firstName, lastName, nickName, phoneNumber, darkestSecret);
             phoneBook.add(contact);
         }
         else if (choice == 2) {
-            std::cout << "Enter the name: " << std::endl;
+            std::cout << "Enter the name: ";
             std::string name;
             std::cin >> name;
             phoneBook.search(name);
+            std::cin.get();
         }
 
     }
