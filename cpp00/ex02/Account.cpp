d1f580fa0cfe966bc3983c11ed67497ee34b73a5
Ajjig <majjig@pm.me>
2022-05-24 19:18:30 +0100

    init working on Account class

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,67 @@
+#include "Account.hpp"
+#include <iostream>
+#include <sys/time.h>
+
+int	Account::getNbAccounts( void ) {
+	return _nbAccounts;
+}
+
+int	Account::getTotalAmount( void ) {
+	return _totalAmount;
+}
+
+int	Account::getNbDeposits( void ) {
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals( void ) {
+	return _totalNbWithdrawals;
+}
+
+int Account::checkAmount() const {
+    return _amount;
+}
+
+void Account::_displayTimestamp() {
+    std::cout << 000;
+}
+
+void Account::displayStatus() const {
+    _displayTimestamp();
+    std::cout << "Account:" << _accountIndex << " | amount:" << _amount << " | deposits:" << _nbDeposits << " | withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+Account::Account(int initial_deposit) {
+    _accountIndex = _nbAccounts;
+    _amount = initial_deposit;
+    _nbDeposits = 0;
+    _nbWithdrawals = 0;
+    _nbAccounts++;
+    _totalAmount += initial_deposit;
+    _totalNbDeposits++;
+}
+
+Account::Account() {
+    _accountIndex = _nbAccounts;
+    _amount = 0;
+    _nbDeposits = 0;
+    _nbWithdrawals = 0;
+    _nbAccounts++;
+}
+
+void	Account::makeDeposit( int deposit ) {
+    _amount += deposit;
+    _nbDeposits++;
+    _totalAmount += deposit;
+    _totalNbDeposits++;
+}
+
+bool Account::makeWithdrawal( int withdrawal ) {
+    if (_amount < withdrawal)
+        return false;
+    _amount -= withdrawal;
+    _nbWithdrawals++;
+    _totalAmount -= withdrawal;
+    _totalNbWithdrawals++;
+    return true;
+}
