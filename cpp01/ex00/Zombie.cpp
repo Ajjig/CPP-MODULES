8ca0666d68cbf1322cb0cc58be3bb6d42b77b4af
Ajjig <majjig@pm.me>
2022-06-05 23:15:03 +0100

    Added Zombie.cpp file

---
+++ cpp01/ex00/Zombie.cpp
@@ -0,0 +1,13 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(string name) {
+    this->name = name;
+}
+
+Zombie::Zombie() {
+    this->name = "";
+}
+
+void Zombie::announce( void ) {
+    std::cout << this -> name << ": Braiiiiiiinnnssss..." << std::endl;
+}
