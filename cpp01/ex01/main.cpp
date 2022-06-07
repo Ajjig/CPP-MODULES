c02dc8d330c69bcf7159b7ad8ec252a04e6831d2
Ajjig <majjig@pm.me>
2022-06-07 23:55:07 +0100

    fix zombiezHorde()

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -3,7 +3,7 @@
 Zombie* zombieHorde( int N, string name ) {
     Zombie *zombies = new Zombie[N];
     for (int i = 0; i < N; i++) {
-        zombies[i] = Zombie(name);
+        zombies[i].setName(name);
     }
     return zombies;
 }
