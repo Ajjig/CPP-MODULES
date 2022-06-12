c8a96a0e24f0d41e436e2d88afee60777a73b131
Ajjig <majjig@pm.me>
2022-06-12 18:26:46 +0100

    Added main() and 2 testcases for testing perposes

---
+++ cpp01/ex03/main.cpp
@@ -0,0 +1,24 @@
+#include "HumanA.hpp"
+
+void testCase1() {
+    Weapon club = Weapon("crude spiked club");
+    HumanA bob("Bob", club);
+    bob.attack();
+    club.setType("some other type of club");
+    bob.attack();
+}
+
+void testCase2() {
+    Weapon club = Weapon("crude spiked club");
+    HumanB jim("Jim");
+    jim.setWeapon(club);
+    jim.attack();
+    club.setType("some other type of club");
+    jim.attack();
+}
+
+int main()
+{
+    testCase1();
+    testCase2();
+}
