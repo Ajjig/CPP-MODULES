0fdf58f06912f4d2e63b5a870741f7cbf4f3e3e4
Ajjig <majjig@pm.me>
2022-06-12 16:26:59 +0100

    Added reference to the str variable

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -4,9 +4,11 @@
 int main( void ) {
     std::string str = "HI THIS IS BRAIN";
     std::string *str_ptr = &str;
+    std::string &str_ref = str;
 
     std::cout << str << std::endl;
-    std::cout << str_ptr << std::endl;
+    std::cout << *str_ptr << std::endl;
+    std::cout << str_ref << std::endl;
 
     return 0;
 }
