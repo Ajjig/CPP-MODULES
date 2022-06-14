2e49f6532e581c3eef35c106f00f48d5d7a7ebeb
Ajjig <majjig@pm.me>
2022-06-14 20:32:47 +0100

    move zombieHorde()

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -1,13 +1,5 @@
 #include "Zombie.hpp"
 
-Zombie* zombieHorde( int N, string name ) {
-    Zombie *zombies = new Zombie[N];
-    for (int i = 0; i < N; i++) {
-        zombies[i].setName(name);
-    }
-    return zombies;
-}
-
 int main( void ) {
     Zombie *zombies = zombieHorde(10, "Zombie");
     for (int i = 0; i < 10; i++) {
