657e92bb4b95e0745eb200036e6a2afdeb226867
Ajjig <majjig@pm.me>
2022-06-14 17:30:27 +0100

    modified .gitignore

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -14,21 +14,22 @@ void Harl::error() {
 }
 
 void Harl::info() {
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn\’t be asking for more!" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
 }
 
 void Harl::warning() {
-    std::cout << "I think I deserve to have some extra bacon for free. I\’ve been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
 }
 
 
 
 void Harl::complain(string level) {
+    typedef void (Harl::*fts_ptr)( void ) ;
     string levels[] = {"debug", "info", "warning", "error"};
-    auto fpointers[] = { Harl::debug, Harl::info, Harl::warning, Harl::error };
+    fts_ptr fts[] = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
     for (int i = 0; i < 4; ++i) {
         if (levels[i] == level) {
-            Harl::fpointers[i]();
+            (this -> *fts[i])();
             return;
         }
     }
