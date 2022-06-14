5d649096c59ea49c344be50b4edeb873baf2a0eb
Ajjig <majjig@pm.me>
2022-06-14 14:51:13 +0100

    Added Harl class

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#define string std::string
+
+class Harl {
+    public :
+        Harl();
+        ~Harl();
+        void complain(string level);
+    private :
+        void info();
+        void warning();
+        void error();
+        void debug();
+}
