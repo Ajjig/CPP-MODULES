2a0a72b4e8907449c7897a1fec4baf42fca3f87a
Ajjig <majjig@pm.me>
2022-06-21 14:39:43 +0100

    Added tests for new gueardGate() function

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -1,9 +1,9 @@
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main( void ) {
-    ClapTrap tom = ClapTrap("tom");
+    ScavTrap tom = ScavTrap("tom");
     tom.setAttackDamage(5);
-    ClapTrap jerry = ClapTrap("jerry");
+    ScavTrap jerry = ScavTrap("jerry");
     tom.attack(jerry.getName()); // tom attacks jerry
     jerry.takeDamage(tom.getAttackDamage()); // jerry takes 5 damage
     jerry.beRepaired(5); // jerry is getting repaired for 5
@@ -11,5 +11,7 @@ int main( void ) {
     jerry.attack(tom.getName()); // jerry revenges tom
     tom.takeDamage(jerry.getAttackDamage()); // tom takes 10 damage // tom is now dead
     tom.attack("Someone else"); // tom cant attack because he is out of HP/
+    tom.guardGate(); // tom is in gate keeper mode
+    jerry.guardGate(); // jerry is in gate keeper mode
     return 0;
 }
