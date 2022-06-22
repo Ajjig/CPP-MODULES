61fbafb194458229ae60e0cf070cec1779cdc641
Ajjig <majjig@pm.me>
2022-06-23 00:03:38 +0100

    Add more tests

--- cpp03/ex03/main.cpp
+++ cpp03/ex03/main.cpp
@@ -6,5 +6,7 @@ int main( void ) {
     dt.highFiveGuys(); // call highFiveGuys()
     dt.guardGate(); // call guardGate()
     dt.whoAmI(); // call DiamondClap unique function
+    dt.takeDamage(100);
+    dt.highFiveGuys(); // can't display message OUT_OF_HP
     return 0;
 }
