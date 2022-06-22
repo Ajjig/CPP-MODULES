e804b2cf69dd60aef977e8ae1dd7f11c27ca49b1
Ajjig <majjig@pm.me>
2022-06-22 21:37:36 +0100

    Add: main():

--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -1,17 +1,17 @@
-#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main( void ) {
-    ScavTrap tom = ScavTrap("tom");
-    tom.setAttackDamage(5);
-    ScavTrap jerry = ScavTrap("jerry");
+    FragTrap tom = FragTrap("tom");
+    tom.setAttackDamage(50);
+    FragTrap jerry = FragTrap("jerry");
     tom.attack(jerry.getName()); // tom attacks jerry
     jerry.takeDamage(tom.getAttackDamage()); // jerry takes 5 damage
-    jerry.beRepaired(5); // jerry is getting repaired for 5
+    jerry.beRepaired(50); // jerry is getting repaired for 5
     jerry.setAttackDamage(10); // jerry's attack damage is now 10
     jerry.attack(tom.getName()); // jerry revenges tom
     tom.takeDamage(jerry.getAttackDamage()); // tom takes 10 damage // tom is now dead
     tom.attack("Someone else"); // tom cant attack because he is out of HP/
-    tom.guardGate(); // tom is in gate keeper mode
-    jerry.guardGate(); // jerry is in gate keeper mode
+    tom.highFiveGuys(); // to can't do anything because of he is out of HP
+    jerry.highFiveGuys(); // jerry will display high fives guys msg
     return 0;
 }
