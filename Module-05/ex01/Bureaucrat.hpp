ccceaa38c89b03224ce08ff6e7c9d143cf804559
Ajjig <majjig@pm.me>
2022-06-26 23:33:20 +0100

    Added ex00 files to ex01

---
+++ Module-05/ex01/Bureaucrat.hpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+#define string std::string
+
+class Bureaucrat {
+    public :
+        Bureaucrat(const string name, int grade);
+        Bureaucrat(const Bureaucrat & obj);
+        Bureaucrat( void );
+        ~Bureaucrat( void );
+        Bureaucrat & operator = (const Bureaucrat & obj);
+        void incrementGrade( void );
+        void decrementGrade( void );
+        void setGrade(int grade);
+        int getGrade( void ) const;
+        string getName( void ) const;
+        void setName(string name);
+        class GradeTooHighException : public std::exception {
+            public :
+                const char * what() const throw();
+        };
+        class GradeTooLowException : public std::exception {
+            public :
+                const char * what() const throw();
+        };
+    private :
+        string _name;
+        int _grade;
+};
+
+std::ostream & operator << (std::ostream & o, const Bureaucrat & obj);
