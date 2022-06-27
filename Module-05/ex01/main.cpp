8564b0070f68a861fe16476f989c95d2d20ee1db
Ajjig <majjig@pm.me>
2022-06-27 20:00:07 +0100

    Added more tests

--- Module-05/ex01/main.cpp
+++ Module-05/ex01/main.cpp
@@ -3,8 +3,14 @@
 
 int main( void ) {
     Bureaucrat b("Bob", 10);
+    std::cout << "======== b ========" << std::endl;
+    std::cout << b;
     Form canSign("canSign", 10, 10);
+    std::cout << "======== canSign ========" << std::endl;
+    std::cout << canSign;
     Form cannotSign("cannotSign", 5, 7);
+    std::cout << "======== cannotSign ========" << std::endl;
+    std::cout << cannotSign;
     b.signForm(canSign);
     b.signForm(cannotSign);
 }
