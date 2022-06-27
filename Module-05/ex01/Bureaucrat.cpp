1281c6f008652291992ab63bcac15d0502baf3e7
Ajjig <majjig@pm.me>
2022-06-27 01:40:07 +0100

    return when an exception

--- Module-05/ex01/Bureaucrat.cpp
+++ Module-05/ex01/Bureaucrat.cpp
@@ -85,6 +85,7 @@ void Bureaucrat::signForm(Form & form) {
     } 
     catch (std::exception & e) {
         std::cout << this -> getName() << " cannot sign " << form.getName() << " because " << e.what() << std::endl;
+        return;
     }
     std::cout << this -> getName() << " signs " << form.getName() << std::endl;
 }
