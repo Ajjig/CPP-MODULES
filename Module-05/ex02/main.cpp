9a441a37c729e5b62ea672c5bef62ada869b5702
Ajjig <majjig@pm.me>
2022-06-28 21:40:36 +0100

    Added more tests

--- Module-05/ex02/main.cpp
+++ Module-05/ex02/main.cpp
@@ -1,16 +1,21 @@
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 int main( void ) {
-    Bureaucrat b("Bob", 10);
-    std::cout << "======== b ========" << std::endl;
-    std::cout << b;
-    Form canSign("canSign", 10, 10);
-    std::cout << "======== canSign ========" << std::endl;
-    std::cout << canSign;
-    Form cannotSign("cannotSign", 5, 7);
-    std::cout << "======== cannotSign ========" << std::endl;
-    std::cout << cannotSign;
-    b.signForm(canSign);
-    b.signForm(cannotSign);
+    RobotomyRequestForm f1("__f1__");
+    PresidentialPardonForm f2("__f2__");
+    ShrubberyCreationForm f3("__f3__");
+    Bureaucrat b1("b1", 1);
+    Bureaucrat b2("b2", 150);
+    std::cout << "============= b1 ============="<< std::endl;
+    b1.executeForm(f1);
+    b1.executeForm(f2);
+    b1.executeForm(f3);
+    std::cout << "============= b2 ============="<< std::endl;
+    b2.executeForm(f1);
+    b2.executeForm(f2);
+    b2.executeForm(f3);
 }
