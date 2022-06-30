ff703ac6c96df346a18202c5dab5dc95ca8f7d62
Ajjig <majjig@pm.me>
2022-06-30 19:48:49 +0100

    Added cout messages

--- Module-05/ex03/Intern.cpp
+++ Module-05/ex03/Intern.cpp
@@ -4,11 +4,13 @@ Intern::Intern() {
     this -> _forms[0] = new RobotomyRequestForm("None");
     this -> _forms[1] = new ShrubberyCreationForm("None");
     this -> _forms[2] = new PresidentialPardonForm("None");
+    std::cout << "Intern constructor called" << std::endl;
     return;
 }
 
 Intern::Intern(Intern const & src) {
     *this = src;
+    std::cout << "Intern copy constructor called" << std::endl;
     return;
 }
 
@@ -18,6 +20,7 @@ Intern & Intern::operator=(Intern const & src) {
             _forms[i] = src._forms[i];
         }
     }
+    std::cout << "Intern assignation operator called" << std::endl;
     return *this;
 }
 
@@ -25,13 +28,14 @@ Intern::~Intern() {
     delete this -> _forms[0];
     delete this -> _forms[1];
     delete this -> _forms[2];
+    std::cout << "Intern destructor called" << std::endl;
 }
 
 Form * Intern::makeForm(string name, string target) {
     string names[] = {"RobotomyRequest", "ShrubberyCreation", "PresidentialPardon"};
     int i = 0;
     for (; name != names[i]; i++) {}
-    return i < 3 ? _forms[i](*forms[i]) : throw Intern::UnknownFormException();
+    return i < 3 ? _forms[i] -> clone(target) : throw Intern::UnknownFormException();
 }
 
 const char * Intern::UnknownFormException::what() const throw() {
