e498ed1b3b3c7d9e3e7ddb8b02591da9d771905e
Ajjig <majjig@pm.me>
2022-06-30 20:31:30 +0100

    Added more tests

--- Module-05/ex03/main.cpp
+++ Module-05/ex03/main.cpp
@@ -3,6 +3,7 @@
 #include "ShrubberyCreationForm.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "Intern.hpp"
 
 int main( void ) {
     RobotomyRequestForm f1("__f1__");
@@ -18,4 +19,35 @@ int main( void ) {
     b2.executeForm(f1);
     b2.executeForm(f2);
     b2.executeForm(f3);
+    std::cout << "========================= EX03 ========================="<< std::endl;
+    Intern intern;
+    Form * form = nullptr;
+    try {
+        form = intern.makeForm("RobotomyRequest", "__target__"); // executes RobotomyRequestForm
+        b1.executeForm(*form);
+        delete form;
+    }
+    catch(std::exception & e) {
+        std::cout << e.what() << std::endl
+        << "exception ^^" << std::endl;
+    }
+    try {
+        form = intern.makeForm("PresidentialPardon", "__target__"); // executes PresidentialPardonForm
+        b1.executeForm(*form);
+        delete form;
+    }
+    catch(std::exception & e) {
+        std::cout << e.what() << std::endl
+        << "exception ^^" << std::endl;
+    }
+    try {
+        form = intern.makeForm("Invalid_one", "__target__"); // throws Intern::UnknownFormException
+        b1.executeForm(*form);
+        delete form;
+    }
+    catch(std::exception & e) {
+        std::cout << e.what() << std::endl
+        << "exception ^^" << std::endl;
+    }
+
 }
