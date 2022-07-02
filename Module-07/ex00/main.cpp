c689f22d6fffbbad9f5b164e9eb8bd392f6aa5c5
Ajjig <majjig@pm.me>
2022-07-02 20:15:17 +0100

    Add: tests to main() function

--- Module-07/ex00/main.cpp
+++ Module-07/ex00/main.cpp
@@ -2,6 +2,23 @@
 
 
 int main( void ) {
+    string sA = "Hello";
+    string sB = "World";
+    int iA = 42;
+    int iB = 69;
+    std::cout << sA << " - " << sB << std::endl;
+    ::swap< string >(sA, sB);
+    std::cout << sA << " - " << sB << std::endl;
 
+    std::cout << iA << " - " << iB << std::endl;
+    ::swap< int >(iA, iB);
+    std::cout << iA << " - " << iB << std::endl;
+
+    float fA = 42.0f;
+    float fB = 69.0f;
+    long long lA = 484573092;
+    long long lB = 6584798949;
+    std::cout << "Min: " << ::min< long long >(lA, lB) << ", " << ::min< float >(fA, fB) << std::endl;
+    std::cout << "Max: " << ::max< long long >(lA, lB) << ", " << ::max< float >(fA, fB) << std::endl;
     return 0;
 }
