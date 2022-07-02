fcd3d3786aa652bf408e25c33e536157eef2d41c
Ajjig <majjig@pm.me>
2022-07-02 20:14:31 +0100

    Add: whatever.hpp

---
+++ Module-07/ex00/whatever.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <iostream>
+#define string std::string
+
+template <typename T>
+void swap(T & a, T & b) {
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(T & a, T & b) {
+    return b > a ? a : b;
+}
+
+template <typename T>
+T max(T & a, T & b) {
+    return b > a ? b : a;
+}
