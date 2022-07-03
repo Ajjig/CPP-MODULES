23d3dc5152e8bd07d70acce423000d1c5f2fdbaf
Ajjig <majjig@pm.me>
2022-07-03 22:43:34 +0100

    Add: main.cpp

---
+++ Module-08/ex01/main.cpp
@@ -0,0 +1,18 @@
+#include "Span.hpp"
+
+int main( void ) {
+    Span s(10);
+    s.addNumber(-1);
+    s.addNumber(-9);
+    s.addNumber(4);
+    s.addNumber(10);
+    s.addNumber(5);
+    s.addNumber(2);
+    s.addNumber(3);
+    s.addNumber(1);
+    s.addNumber(-6);
+    s.addNumber(7);
+    std::cout << "shortestSpan: " << s.shortestSpan() << std::endl; // this should print 1
+    std::cout << "longestSpan: " << s.longestSpan() << std::endl;   // this should print 19
+
+}
