17beaf9c13f7f0e5b9d7a0658a39c000d2b2d321
Ajjig <majjig@pm.me>
2022-07-03 23:41:28 +0100

    Add: MutantStack class

---
+++ Module-08/ex02/MutantStack.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <stack>
+
+template < typename T >
+class MutantStack : public std::stack<T> {
+    public :
+        MutantStack() : std::stack<T>() {}
+        ~MutantStack() {}
+        MutantStack(const MutantStack & src) : std::stack<T>(src) {}
+        MutantStack & operator=(const MutantStack & src) {
+            *this = src;
+            return *this;
+        }
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator begin() { return this->c.begin(); }
+        iterator end() { return this->c.begin(); }        
+};
